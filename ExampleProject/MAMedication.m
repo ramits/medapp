3208bfd0e612b5628934ed61e5e0ff4ce9cf53c6
Joseph Constantakis <jcon5294@gmail.com>
2014-10-29 14:02:34 -0400

    make MAMedication model object

---
+++ ExampleProject/MAMedication.m
@@ -0,0 +1,13 @@
+//
+//  MAMedication.m
+//  ExampleProject
+//
+//  Created by Joseph Constantakis on 10/29/14.
+//  Copyright (c) 2014 Jinatum. All rights reserved.
+//
+
+#import "MAMedication.h"
+
+@implementation MAMedication
+
+@end
